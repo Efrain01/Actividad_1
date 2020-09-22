1fe6883119b863d43c3e6791b080988e75730531
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-09-22 18:29:47 -0500

    Lee e imprime todo

--- main.c
+++ main.c
@@ -5,15 +5,18 @@ int main(){
     int edad, dia,mes,anio;
     printf("Ingrese su nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
+    fflush;
     printf("Porfavor ingrese su edad: ");
     scanf("%i",&edad);
     printf("Ingrese su dia de nacimiento: ");
     scanf("%i",&dia);
     printf("Ingresa el numero del mes de tu nacimiento: ");
-    scanf("%i",mes);
-    printf("Ingrese el anio de tu cumpleaños: ");
+    scanf("%i",&mes);
+    fflush;
+    printf("Ingresa el anio de tu nacimiento: ");
     scanf("%i",&anio);
-    printf("Hola %s Tu vas a cumplir %i",nombre, edad+1);
+    printf("Hola %s:Tu vas a cumplir %i",nombre, edad+1);
+    printf(" En el %i",edad+1+anio);
     return 0;
  }
  
