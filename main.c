084b467ecf2217422b4449ecf71b50d8fe68f3f4
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-09-22 17:58:46 -0500

    Lee el año de cumpleanios y lo imprime

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main(){
     char nombre[100];
-    int edad, dia,mes;
+    int edad, dia,mes,anio;
     printf("Ingrese su nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Porfavor ingrese su edad: ");
@@ -11,6 +11,8 @@ int main(){
     scanf("%i",&dia);
     printf("Ingresa el numero del mes de tu nacimiento: ");
     scanf("%i",mes);
+    printf("Ingrese el anio de tu cumpleaños: ");
+    scanf("%i",&anio);
     printf("Hola %s Tu vas a cumplir %i",nombre, edad+1);
     return 0;
  }
