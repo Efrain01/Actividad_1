59f5480600c50211748babc8cf4e514b922f4e8c
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-09-22 17:43:08 -0500

    Lee el dia de su cumpleanios y la imprime

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 int main(){
     char nombre[100];
-    int edad;
+    int edad, dia;
     printf("Ingrese su nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Porfavor ingrese su edad: ");
     scanf("%i",&edad);
-    printf("Hola %s y vas a cumplir %i",nombre, edad+1);
+    printf("Ingrese su dia de nacimiento: ");
+    scanf("%i",&dia);
+    printf("Hola %s Tu vas a cumplir %i",nombre, edad+1);
     return 0;
  }
  
