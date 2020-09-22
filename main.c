8cd1b222ef0a642bb08b1b11f5f1f849ac431b9d
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-09-22 17:55:18 -0500

    Lee el numero del mes de su cumpleaños y lo imprime

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 int main(){
     char nombre[100];
-    int edad, dia;
+    int edad, dia,mes;
     printf("Ingrese su nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Porfavor ingrese su edad: ");
     scanf("%i",&edad);
     printf("Ingrese su dia de nacimiento: ");
     scanf("%i",&dia);
+    printf("Ingresa el numero del mes de tu nacimiento: ");
+    scanf("%i",mes);
     printf("Hola %s Tu vas a cumplir %i",nombre, edad+1);
     return 0;
  }
